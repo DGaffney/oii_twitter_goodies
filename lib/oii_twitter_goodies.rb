8b5b2badca919d4abe62bc118f1ff472522500e6
Devin Gaffney <itsme@devingaffney.com>
2013-03-14 10:59:47 -0700

    internet

--- lib/oii_twitter_goodies.rb
+++ lib/oii_twitter_goodies.rb
@@ -1,20 +1,16 @@
-require 'bundler'
-require 'mongo_mapper'
-require 'oauth'
-require 'twitter'
-require 'tweetstream'
-require 'hashie'
-require 'typhoeus'
-require 'json'
-
-require "oii_twitter_goodies/version"
-
-Dir['oii_twitter_goodies/extensions/*.rb'].each {|file| require file }
-Dir['oii_twitter_goodies/lib/*.rb'].each {|file| require file }
-Dir['oii_twitter_goodies/model/*.rb'].each {|file| require file }
-
-
-
 module OIITwitterGoodies
-  # Your code goes here...
+  require 'bundler'
+  require 'mongo_mapper'
+  require 'oauth'
+  require 'twitter'
+  require 'tweetstream'
+  require 'hashie'
+  require 'typhoeus'
+  require 'json'
+
+  require "oii_twitter_goodies/version"
+
+  Dir['oii_twitter_goodies/extensions/*.rb'].each {|file| require file }
+  Dir['oii_twitter_goodies/lib/*.rb'].each {|file| require file }
+  Dir['oii_twitter_goodies/model/*.rb'].each {|file| require file }
 end
