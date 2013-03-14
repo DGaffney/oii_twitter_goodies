14194b0da41fc0962f6b952949f12d12d4f876cc
Devin Gaffney <itsme@devingaffney.com>
2013-03-14 10:20:29 -0700

    internet

--- lib/oii_twitter_goodies.rb
+++ lib/oii_twitter_goodies.rb
@@ -9,9 +9,9 @@ require 'json'
 
 require "oii_twitter_goodies/version"
 
-Dir[File.dirname(__FILE__) + '/oii_twitter_goodies/extensions/*.rb'].each {|file| require file }
-Dir[File.dirname(__FILE__) + '/oii_twitter_goodies/lib/*.rb'].each {|file| require file }
-Dir[File.dirname(__FILE__) + '/oii_twitter_goodies/model/*.rb'].each {|file| require file }
+Dir['oii_twitter_goodies/extensions/*.rb'].each {|file| require file }
+Dir['oii_twitter_goodies/lib/*.rb'].each {|file| require file }
+Dir['oii_twitter_goodies/model/*.rb'].each {|file| require file }
 
 
 
